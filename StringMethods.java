35bb652a48dc736c0d50d856b06c3d087fddcf2e
Arunasasindran <103830341+Arunasasindran@users.noreply.github.com>
2022-04-23 11:10:21 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,41 @@
+package week2.day1.classroom;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+
+		StringMethods obj = new StringMethods();
+		obj.printLetter_e();
+		System.out.println("--------------");
+		obj.printWordsStartsWith_T();
+	}
+
+	public void printLetter_e() {
+
+		String text0 = "testleaf";
+		char[] charArray1 = text0.toCharArray();
+		for (char i : charArray1) {
+			if (i == 'e') {
+				System.out.println(i);
+			}
+		}
+
+	}
+	
+	public void printWordsStartsWith_T()
+	{
+	String text1 = "Testleaf is situated in twin towers teynampet";
+	String[] words = text1.split(" ");
+	for(String wordTort : words) {
+		if(wordTort.startsWith("T"))
+		{
+			System.out.println(wordTort);
+		}
+		else if (wordTort.startsWith("t")) {
+			System.out.println(wordTort);
+		}
+	}
+	
+	}
+
+}
